b637d6b6120552b436d1884d777236391ceb1d3e
kim-kiwi <s26018@gsm.hs.kr>
2026-04-27 12:10:15 +0900

    add: 테스트용으로 about 페이지 추가.
    변경 내역
     - add:   src/routes/+layout.svelte home, about navbar 추가
     - add:   src/routes/about/+page.svelte about 페이지

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,30 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.png';
+  import favicon from "$lib/assets/favicon.png";
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
-	<title>Toolnet</title>
+  <link rel="icon" href={favicon} />
+  <title>Toolnet</title>
 </svelte:head>
 
+<header>
+  <a href="/">home</a>
+  <a href="/about">about</a>
+</header>
 {@render children()}
+
+<style>
+  :global(body) {
+    margin: 1px;
+    padding: 1px;
+    background-color: #282828;
+	overflow: hidden;
+  }
+  header {
+    position: fixed;
+    margin: 0;
+    padding: 0;
+  }
+</style>
